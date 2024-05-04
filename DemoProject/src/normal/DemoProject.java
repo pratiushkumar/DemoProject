5823e46da529819dc93c2bb55d70ac97081bde89
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2024-05-04 15:27:40 +0530

    THIRD COMMIT

--- DemoProject/src/normal/DemoProject.java
+++ DemoProject/src/normal/DemoProject.java
@@ -4,7 +4,9 @@ public class DemoProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+StringBuffer sb=new StringBuffer("viratkohli");
+int length=sb.append("INDRCB").reverse().length();
+System.out.println(length);
 	}
 
 }
